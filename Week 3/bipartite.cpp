bbd91cb14d3e0612d8d6e05b018bdae666ba4b0a
Alexander Ding <alding@commschool.org>
2017-08-16 18:21:17 -0400

    Week 3

--- Week 3/bipartite.cpp
+++ Week 3/bipartite.cpp
@@ -5,34 +5,41 @@
 using std::vector;
 using std::queue;
 
-int distance(vector<vector<int> > &adj, int s, int t) {
-  // -1 = not reached
-  vector<int> dist(adj.size(), -1);
+int flip(int i) {
+  if (i == 0) {
+    return 1;
+  } else {
+    return 0;
+  }
+}
+
+int bipartite(vector<vector<int> > &adj) {
+  // -1 = not reached; 0 & 1 are group numbers
+  vector<int> group(adj.size(), -1);
   queue<int> nodes;
 
-  // initialize
-  dist[s] = 0; // dist from s to s is 0
+  if (adj.size() < 2) return 0; // well you need two nodes to make a bipartite
 
-  if (s == t) return 0;
-  
-  nodes.push(s);
+  // initialize
+  group[0] = 0; // first node is group 0
+  nodes.push(0);
   while (!nodes.empty()) {
     // get top node from queue
     int u = nodes.front(); nodes.pop();
-
     // loop through all neighbors
     int m = adj[u].size();
     for (int i = 0; i < m; i++) {
       // if neighbor not found before
-      if (dist[adj[u][i]] == -1) {
-        dist[adj[u][i]] = dist[u] + 1; // or ++dist[u]
+      if (group[adj[u][i]] == -1) {
+        group[adj[u][i]] = flip(group[u]);
         nodes.push(adj[u][i]); // add to queue for next layer
-
-        if (adj[u][i] == t) return dist[adj[u][i]];
+      } else {
+        // if found, check if breaks rule
+        if (group[adj[u][i]] == group[u]) return 0;
       }
     }
   }
-  return -1;
+  return 1;
 }
 
 int main() {
@@ -45,8 +52,5 @@ int main() {
     adj[x - 1].push_back(y - 1);
     adj[y - 1].push_back(x - 1);
   }
-  int s, t;
-  std::cin >> s >> t;
-  s--, t--;
-  std::cout << distance(adj, s, t);
+  std::cout << bipartite(adj);
 }
